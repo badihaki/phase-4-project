b2c85e10619301376dcb4695274ce20767a6a4e7
Badi-Haki Martin <jojackblack@gmail.com>
2022-10-11 10:32:09 -0400

    fleshed out user controller with index, show, create, find_user, and permitted parameter functs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+
+    def index
+        render json: User.all, status: :ok
+    end
+
+    def show
+        user = find_user
+        render json: user, status: :ok
+    end
+
+    def create
+        user = User.create!(create_params)
+        render json: user, status: :created
+    rescue ActiveRecord::RecordInvalid => error
+        render json: {errors: error.record.errors}
+    end
+
+    private
+
+    def find_user
+        User.find(params[:id])
+    end
+
+    def create_params
+        params.permit(:email, :password, :password_confirmation, :nickname, :bio)
+    end
+
+    def update_params
+        params.permit(:bio)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
+    # associations
+    
+    #validations
 end
