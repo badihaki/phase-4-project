ada8dee9054d9ed8d9c6e0a6711c5f4f2d313c11
Badi-Haki Martin <jojackblack@gmail.com>
2022-10-13 01:42:40 -0400

    removed admin from users in response to feedback from Nancy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_13_030424) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_13_054124) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,7 +28,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_13_030424) do
     t.string "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin"
   end
 
 end
