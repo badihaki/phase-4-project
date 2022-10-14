55d178557a778a69abec4408feb4d965b5806b66
Badi-Haki Martin <jojackblack@gmail.com>
2022-10-14 02:25:52 -0400

    added state to newGameForm.js to make form stateful. Expanded genredropdown.js with options

---
+++ client/src/Components/Games/GenreDropdown.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function GenreDropdown(){
+    return(
+        <div>
+            <select>
+                <option value={""}>Genre</option>
+                <option value={"Action"}>Action</option>
+            </select>
+        </div>
+    )
+}
--- client/src/Components/Games/NewGameForm.js
+++ client/src/Components/Games/NewGameForm.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NewGameForm(){
+
+    const [ gameName, setGameName ] = useState("");
+    const [ gameGenre, setGameGenre ] = useState("");
+
+    function handleNameChange(e){
+        const value = e.target.value;
+        setGameName(value);
+    }
+
+    function handleGenreChange(e){
+        const value = e.target.value;
+        setGameGenre(value);
+    }
+
     return(
         <div>
             <form>
-                <input name="name" placeholder="Game Name" />
+                <input name="name" placeholder="Game Name" value={gameName} onChange={handleNameChange} />
                 <button type="submit">Add game</button>
             </form>
         </div>
