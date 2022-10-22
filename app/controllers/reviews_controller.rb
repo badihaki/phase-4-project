22b743bc78189ea5aaeed92c312d4a813921d639
Badi-Haki Martin <jojackblack@gmail.com>
2022-10-22 12:43:46 -0400

    reviews controller is finished. finished writing logic for create, update and destroy actions. wrote some private methods to find resources, authorize the request, and render a response if a resource aint found

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,8 @@
 class ReviewsController < ApplicationController
 
     before_action :authorize
+    skip_before_action :authorize, only: [:index, :show]
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
 
     def index
         user = find_user()
@@ -8,21 +10,39 @@ class ReviewsController < ApplicationController
     end
 
     def show
-        # return render json: {error: "Not Authorized"}, status: :unauthorized unless session.include? :user_id # <<-- use this if below doesn't work
-        return render json: {error: "Not Authorized"}, status: :unauthorized unless sessions.include? :user_id
-        
+        review = find_review()
+        render json: review, status: :ok
     end
 
     def create
+        user = find_user()
+        new_review = user.reviews.create!(permitted_params)
+        render json: new_review, status: :created
+    rescue ActiveRecord::RecordInvalid => e
+        render json: {errors: e.record.errors}, status: :unprocessable_entity
     end
 
     def update
+        review = find_review()
+        review.update!(permitted_params)
+        render json: review, status: :accepted
     end
 
     def destroy
+        review = find_review()
+        review.destroy!
+        head :no_content
     end
 
     private
+
+    def permitted_params
+        params.permit(:game_id, :score, :comment)
+    end
+
+    def find_review
+        Review.find(id:params[:id])
+    end
 
     def find_user
         User.find(id:sessions[:user_id])
@@ -30,6 +50,10 @@ class ReviewsController < ApplicationController
 
     def authorize
         render json: {error: "Not authorized"}, status: :unauthorized, unless session.include? :user_id
+    end
+
+    def not_found_response
+        render json: {error: "Record not found"}, status: :not_found
     end
 
 end
